55fa33e642685b79b6c15cd7183ef4031aeebcb6
Gustavo Volke <gustavo_volke@hotmail.com>
2025-02-13 21:24:13 -0300

    design patterns and tests

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,17 +1,26 @@
 import { hash } from "bcryptjs"
 import { UsersRepository } from "@/repositories/users-repository"
 import { UserAlreadyExistsError } from "./errors/user-alreadt-exists-error"
+import { User } from "@prisma/client"
 
-interface RegisterUseCaseRequest {
+interface RegisterServiceRequest {
   name: string
   email: string
   password: string
 }
 
+interface RegisterServiceResponse {
+  user: User
+}
+
 export class RegisterService {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ email, name, password }: RegisterUseCaseRequest) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -23,10 +32,12 @@ export class RegisterService {
     // Inversão de dependências. Não instanciar dependências dentro do service.
     // const prismaUsersRepository = new PrismaUsersRepository()
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
